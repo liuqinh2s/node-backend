a14f3fb87d607e638affd42ff5116f400149080b
liuqinh2s <liuqinh2s@gmail.com>
2021-09-26 08:34:04 +0800

    项目初始化

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get("/", function (req, res) {
   res.send("Hello world!");
 });
 app.listen(3000);
-console.log("started");
-console.log("http://localhost:3000");
+console.log("server started");
+console.log("address: http://localhost:3000");
